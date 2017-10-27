3b27bdfd5cb8897b3085f2f33141a979a355a335
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-27 16:04:25 +0200

    chore: add release scripts

---
+++ bin/publish.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+
+readonly CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != master ]; then
+  echo "You must be on 'master' branch to publish a release, aborting..."
+  exit 1
+fi
+
+if ! git diff-index --quiet HEAD --; then
+  echo "Working tree is not clean, aborting..."
+  exit 1
+fi
+
+if ! yarn; then
+  echo "Failed installing dependencies, aborting..."
+  exit 1
+fi
+
+if ! yarn test; then
+  echo "Tests failed, aborting..."
+  exit 1
+fi
+
+if ! yarn run build; then
+  echo "Build failed, aborting..."
+  exit 1
+fi
+
+readonly PACKAGE_VERSION=$(< package.json grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[:space:]')
+
+npm publish
+
+git tag "v$PACKAGE_VERSION"
+
+git push --tags
+
+echo "Pushed package to npm, and also pushed 'v$PACKAGE_VERSION' tag to git repository."
